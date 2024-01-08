b4e925d819eb177b4e0de769d369c2c2d9044dd2
bobacktech <bobacktech@gmail.com>
2024-01-07 17:34:57 -0800

    SIM-7: Added pydoc comments to the code.

--- augmented_skateboarding_simulator/sim_time_manager.py
+++ augmented_skateboarding_simulator/sim_time_manager.py
@@ -2,26 +2,64 @@ import time
 
 
 class SimTimeManager:
+    """
+    A class to manage simulation time.
+
+    This class provides functionalities to set and manage time steps for simulations,
+    track the start and elapsed time, and reset the simulation time settings.
+
+    Attributes:
+        __time_step_ms (int): The time step for the simulation in milliseconds.
+        __sim_start_time_ms (int): The start time of the simulation in milliseconds.
+    """
+
     __time_step_ms = 0
     __sim_start_time_ms = 0
 
     def __init__(self):
+        """Initializes the SimTimeManager class."""
         pass
 
     def set_sim_time_step(self, step_ms):
+        """
+        Sets the time step for the simulation.
+
+        The time step can only be set once and is immutable thereafter.
+
+        Args:
+            step_ms (int): The time step in milliseconds.
+        """
         if SimTimeManager.__time_step_ms == 0:
             SimTimeManager.__time_step_ms = step_ms
 
     def sim_time_step(self) -> int:
+        """Returns the current simulation time step in milliseconds."""
         return SimTimeManager.__time_step_ms
 
     def start_sim(self):
+        """
+        Starts the simulation timer.
+
+        Sets the start time of the simulation in milliseconds,
+        but only if it has not already been set.
+        """
         if SimTimeManager.__sim_start_time_ms == 0:
             SimTimeManager.__sim_start_time_ms = int(time.time_ns() // 1000000)
 
     def sim_elapsed_time(self) -> int:
+        """
+        Calculates the elapsed time since the start of the simulation.
+
+        Returns:
+            int: The elapsed time in milliseconds.
+        """
         return int(time.time_ns() // 1000000) - SimTimeManager.__sim_start_time_ms
 
     def reset_sim(self):
+        """
+        Resets the simulation time settings.
+
+        This method resets both the simulation start time and the time step to zero.
+        """
         SimTimeManager.__sim_start_time_ms = 0
         SimTimeManager.__time_step_ms = 0
--- tests/test_sim_time_manager.py
+++ tests/test_sim_time_manager.py
@@ -3,11 +3,27 @@ import time
 
 
 class TestSimTimeManager:
+    """
+    Unit test class for SimTimeManager.
+
+    This class contains several test methods to validate the functionality of the SimTimeManager class.
+    """
+
     def test_initialization(self):
+        """
+        Tests the initialization of SimTimeManager.
+
+        Ensures that an instance of SimTimeManager is successfully created and is not None.
+        """
         sim_manager = SimTimeManager()
         assert sim_manager is not None, "Failed to initialize SimTimeManager."
 
     def test_set_and_get_time_step(self):
+        """
+        Tests setting and getting the time step in SimTimeManager.
+
+        Verifies that the time step is correctly set and retrieved by the sim_time_step method.
+        """
         sim_manager = SimTimeManager()
         time_step = 100  # 100 ms
         sim_manager.set_sim_time_step(time_step)
@@ -16,6 +32,11 @@ class TestSimTimeManager:
         ), "Time step did not set correctly."
 
     def test_set_time_step_only_once(self):
+        """
+        Tests the immutability of the time step in SimTimeManager.
+
+        Ensures that the time step, once set, cannot be changed to a different value.
+        """
         sim_manager = SimTimeManager()
         initial_time_step = 100
         new_time_step = 200
@@ -26,6 +47,11 @@ class TestSimTimeManager:
         ), "Time step should not change after being set once."
 
     def test_start_sim(self):
+        """
+        Tests the start of the simulation timing in SimTimeManager.
+
+        Checks that the simulation start time is set (greater than zero) when start_sim is called.
+        """
         sim_manager = SimTimeManager()
         sim_manager.start_sim()
         assert (
@@ -33,10 +59,14 @@ class TestSimTimeManager:
         ), "Simulation start time should be set."
 
     def test_sim_elapsed_time(self):
+        """
+        Tests the calculation of elapsed time in SimTimeManager.
+
+        Verifies that the elapsed time since the start of the simulation is correctly calculated.
+        This is done by sleeping the thread for a known duration and checking if the elapsed time is as expected.
+        """
         sim_manager = SimTimeManager()
         sim_manager.reset_sim()
-        x = SimTimeManager._SimTimeManager__sim_start_time_ms
-
         sim_manager.start_sim()
         time.sleep(0.1)  # Sleep for 100ms
         elapsed_time = sim_manager.sim_elapsed_time()
