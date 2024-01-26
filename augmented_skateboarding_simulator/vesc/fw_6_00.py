3d63be00d80e8fb00b9586fded3892dde6593a54
bobacktech <bobacktech@gmail.com>
2024-01-26 13:06:40 -0800

    SIM-17: Added a firmware message class.

---
+++ augmented_skateboarding_simulator/vesc/fw_6_00.py
@@ -0,0 +1,17 @@
+class FirmwareMessage:
+    """
+    See comm/commands.c in VESC bldc-6.00 source code on Github for message specification.
+    """
+
+    BYTE_LENGTH = 65
+
+    def __init__(self) -> None:
+        self.__buffer = bytearray(FirmwareMessage.BYTE_LENGTH)
+        self.__buffer[0] = 0
+        self.__buffer[1] = 6
+        self.__buffer[2] = 00
+        self.__buffer[3:14] = "HardwareName".encode("utf-8")
+
+    @property
+    def buffer(self):
+        return bytes(self.__buffer)
