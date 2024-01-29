a32fa4aaca7dc1429c9719669ef23c86b178c9fa
bobacktech <bobacktech@gmail.com>
2024-01-28 19:19:02 -0800

    SIM-17:  Added comments to class FirmwareMessage.

--- augmented_skateboarding_simulator/vesc/fw_6_00.py
+++ augmented_skateboarding_simulator/vesc/fw_6_00.py
@@ -1,11 +1,19 @@
 class FirmwareMessage:
     """
-    See comm/commands.c in VESC bldc-6.00 source code on Github for message specification.
+    See the message specification in [commands.c](https://github.com/vedderb/bldc/blob/6.00/comm/commands.c)
+    in VESC bldc-6.00 source code on Github.
+
     """
 
     BYTE_LENGTH = 65
 
     def __init__(self) -> None:
+        """
+        Initializes a new instance of the FirmwareMessage class.
+
+        Sets up the firmware message buffer according to the specified format.
+        The buffer is initialized with predefined values, and a section is filled with an encoded string.
+        """
         self.__buffer = bytearray(FirmwareMessage.BYTE_LENGTH)
         self.__buffer[0] = 0
         self.__buffer[1] = 6
@@ -14,4 +22,10 @@ class FirmwareMessage:
 
     @property
     def buffer(self):
+        """
+        The buffer property representing the immutable form of the firmware message.
+
+        Returns:
+            bytes: An immutable bytes object representing the current state of the firmware message buffer.
+        """
         return bytes(self.__buffer)
