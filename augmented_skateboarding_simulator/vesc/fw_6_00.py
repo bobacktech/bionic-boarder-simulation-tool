101eba59070fd4d5685465e50ce4dd208ad4ae17
bobacktech <bobacktech@gmail.com>
2024-01-29 21:37:32 -0800

    SIM-15: Added the StateMessage class with only the data fields required by the simulation.

--- augmented_skateboarding_simulator/vesc/fw_6_00.py
+++ augmented_skateboarding_simulator/vesc/fw_6_00.py
@@ -28,3 +28,45 @@ class FirmwareMessage:
             bytes: An immutable bytes object representing the current state of the firmware message buffer.
         """
         return bytes(self.__buffer)
+
+
+class StateMessage:
+    BYTE_LENGTH = 76
+
+    def __init__(self) -> None:
+        self.__duty_cycle = 0
+        self.__rpm = 0
+        self.__motor_current = 0
+        self.__input_voltage = 0
+
+    @property
+    def duty_cycle(self):
+        return self.__duty_cycle
+
+    @duty_cycle.setter
+    def duty_cycle(self, value):
+        self.__duty_cycle = value
+
+    @property
+    def rpm(self):
+        return self.__rpm
+
+    @rpm.setter
+    def rpm(self, value):
+        self.__rpm = value
+
+    @property
+    def motor_current(self):
+        return self.__motor_current
+
+    @motor_current.setter
+    def motor_current(self, value):
+        self.__motor_current = value
+
+    @property
+    def input_voltage(self):
+        return self.__input_voltage
+
+    @input_voltage.setter
+    def input_voltage(self, value):
+        self.__input_voltage = value
