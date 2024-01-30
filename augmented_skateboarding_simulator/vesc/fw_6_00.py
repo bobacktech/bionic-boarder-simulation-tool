c69d4f3e0d6c84f0019423c2ce235db88a3aa416
bobacktech <bobacktech@gmail.com>
2024-01-29 21:59:25 -0800

    SIM-15: Added specific type info for the class data members.

--- augmented_skateboarding_simulator/vesc/fw_6_00.py
+++ augmented_skateboarding_simulator/vesc/fw_6_00.py
@@ -1,3 +1,6 @@
+import struct
+
+
 class FirmwareMessage:
     """
     See the message specification in [commands.c](https://github.com/vedderb/bldc/blob/6.00/comm/commands.c)
@@ -31,42 +34,48 @@ class FirmwareMessage:
 
 
 class StateMessage:
-    BYTE_LENGTH = 76
+    BYTE_LENGTH: int = 76
 
     def __init__(self) -> None:
-        self.__duty_cycle = 0
-        self.__rpm = 0
-        self.__motor_current = 0
-        self.__input_voltage = 0
+        self.__duty_cycle: float = 0
+        self.__rpm: int = 0
+        self.__motor_current: float = 0
+        self.__input_voltage: float = 0
 
     @property
-    def duty_cycle(self):
+    def buffer(self) -> bytearray:
+        buffer = bytearray(StateMessage.BYTE_LENGTH)
+        # TBD - serialize data items to byte buffer
+        return buffer
+
+    @property
+    def duty_cycle(self) -> float:
         return self.__duty_cycle
 
     @duty_cycle.setter
-    def duty_cycle(self, value):
+    def duty_cycle(self, value: float) -> None:
         self.__duty_cycle = value
 
     @property
-    def rpm(self):
+    def rpm(self) -> int:
         return self.__rpm
 
     @rpm.setter
-    def rpm(self, value):
+    def rpm(self, value: int) -> None:
         self.__rpm = value
 
     @property
-    def motor_current(self):
+    def motor_current(self) -> float:
         return self.__motor_current
 
     @motor_current.setter
-    def motor_current(self, value):
+    def motor_current(self, value: float) -> None:
         self.__motor_current = value
 
     @property
-    def input_voltage(self):
+    def input_voltage(self) -> float:
         return self.__input_voltage
 
     @input_voltage.setter
-    def input_voltage(self, value):
+    def input_voltage(self, value: float) -> None:
         self.__input_voltage = value
