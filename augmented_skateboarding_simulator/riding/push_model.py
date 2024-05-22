697bb91bfa27831c990f9a388db962cd8c3c3985
bobacktech <bobacktech@gmail.com>
2024-05-22 13:05:58 -0700

    SIM-43: Changed all length units to meters and all time units to seconds.

--- augmented_skateboarding_simulator/riding/push_model.py
+++ augmented_skateboarding_simulator/riding/push_model.py
@@ -24,21 +24,21 @@ class PushModel:
 
     GRAVITY_MPS2 = 9.81
 
-    PUSH_TIME_MS = 500
+    PUSH_TIME_S = 0.5
     """
     Typical push time for a skateboard rider to make contact with the ground to accelerate the skateboard
     """
-    INITIAL_SLOWDOWN_TIME_MS = 50
+    INITIAL_SLOWDOWN_TIME_S = 0.05
     """
-    The time in milliseconds when the force is in the negative x-axis direction.
+    The time in seconds that the skateboard is slowed down when the force is in the negative x-axis direction.
     """
 
     def __init__(self, eboard: EBoard):
         self.eboard = eboard
-        self.__elapsed_time_ms: int = 0
+        self.__elapsed_time_s: int = 0
         self.__push_active = False
 
-    def setup(self, velocity_increase_kph: float, slope_angle_deg: float):
+    def setup(self, velocity_increase_mps: float, slope_angle_deg: float):
         """
         F_total = F_gravity + F_rider
 
@@ -52,43 +52,43 @@ class PushModel:
         simply as a small percentage of the total force.
 
         Args:
-            velocity_increase_kph: the desired velocity increase in kph for this current PUSH
+            velocity_increase_mps: the desired velocity increase in kph for this current PUSH
             slope_angle_deg: the angle of the slope in degrees. Ranges between +90 and -90 degrees
         """
         self.__push_active = True
-        self.__elapsed_time_ms = 0
+        self.__elapsed_time_s = 0
         F_gravity_x_axis_N = (
             self.eboard.total_weight_with_rider_kg
             * self.GRAVITY_MPS2
             * math.sin(math.radians(slope_angle_deg))
         )
         F_rider_x_axis_N = self.eboard.total_weight_with_rider_kg * (
-            velocity_increase_kph / self.PUSH_TIME_MS
+            velocity_increase_mps / self.PUSH_TIME_S
         )
         self.__F_total_N = F_rider_x_axis_N + F_gravity_x_axis_N
         self.__F_slowdown_N = 0.10 * self.__F_total_N
 
-    def step(self, time_step_ms: int) -> float:
+    def step(self, time_step_s: int) -> float:
         """
         This method returns the velocity change in m/s when the force is applied for
-        time_step_ms duration.
+        time_step_s duration.
         """
         if not self.__push_active:
             return None
 
         velocity_delta_mps = None
-        if self.__elapsed_time_ms < self.INITIAL_SLOWDOWN_TIME_MS:
+        if self.__elapsed_time_s < self.INITIAL_SLOWDOWN_TIME_S:
             neg_acceleration_mps2 = (
                 self.__F_slowdown_N / self.eboard.total_weight_with_rider_kg
             )
-            velocity_delta_mps = neg_acceleration_mps2 * (time_step_ms / 1000.0)
+            velocity_delta_mps = neg_acceleration_mps2 * time_step_s
         else:
             acceleration_mps2 = (
                 self.__F_total_N / self.eboard.total_weight_with_rider_kg
             )
-            velocity_delta_mps = acceleration_mps2 * (time_step_ms / 1000.0)
+            velocity_delta_mps = acceleration_mps2 * time_step_s
 
-        self.__elapsed_time_ms += time_step_ms
-        if self.__elapsed_time_ms >= self.PUSH_TIME_MS:
+        self.__elapsed_time_s += time_step_s
+        if self.__elapsed_time_s >= self.PUSH_TIME_S:
             self.__push_active = False
         return velocity_delta_mps
