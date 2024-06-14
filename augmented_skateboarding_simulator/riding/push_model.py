94985565d65b7c4fc797062ac3ecce5a4fd1182c
bobacktech <bobacktech@gmail.com>
2024-06-14 14:01:38 -0700

    SIM-43: Reworked the push_model class.

--- augmented_skateboarding_simulator/riding/push_model.py
+++ augmented_skateboarding_simulator/riding/push_model.py
@@ -24,13 +24,15 @@ class PushModel:
 
     GRAVITY_MPS2 = 9.81
 
-    PUSH_TIME_S = 0.5
+    PUSH_TIME_S = 0.45
     """
-    Typical push time for a skateboard rider to make contact with the ground to accelerate the skateboard
+    Typical push time in seconds for a skateboard rider to accelerate the skateboard with his foot/paddle.
     """
+
     INITIAL_SLOWDOWN_TIME_S = 0.05
     """
-    The time in seconds that the skateboard is slowed down when the force is in the negative x-axis direction.
+    The time in seconds that the skateboard is slowed down when the rider makes initial contact with 
+    the ground with his foot/paddle.  
     """
 
     def __init__(self, eboard: EBoard):
@@ -40,12 +42,13 @@ class PushModel:
 
     def setup(self, velocity_increase_mps: float, slope_angle_deg: float):
         """
-        F_total = F_gravity + F_rider
+        F_rider = F_gravity + F_net
 
-        F_total is the total force applied to the skateboard to increase the skateboard's velocity
-        to velocity_increase_kph over the time interval of PUSH_TIME_MS.
+        F_rider is the total force required by the rider to increase the skateboard's velocity to
+        velocity_increase_mps over the time interval of PUSH_TIME_S.
         F_gravity is the force due to gravity along the x-axis of the skateboard.
-        F_rider is the force due to the rider pushing the skateboard.
+        F_net is the force required to increase the skateboard's velocity to velocity_increase_mps over
+        the time interval of PUSH_TIME_S.
 
         The force right when the foot/paddle makes contact with the ground is in the negative x-axis direction.
         This causes a slight slowing down of the skateboard's velocity. This negative x-axis force is modeled
@@ -62,33 +65,72 @@ class PushModel:
             * self.GRAVITY_MPS2
             * math.sin(math.radians(slope_angle_deg))
         )
-        F_rider_x_axis_N = self.eboard.total_weight_with_rider_kg * (
+        F_net_x_axis_N = self.eboard.total_weight_with_rider_kg * (
             velocity_increase_mps / self.PUSH_TIME_S
         )
-        self.__F_total_N = F_rider_x_axis_N + F_gravity_x_axis_N
-        self.__F_slowdown_N = 0.10 * self.__F_total_N
-
-    def step(self, time_step_s: int) -> float:
+        self.__F_rider_x_axis_N = F_net_x_axis_N + F_gravity_x_axis_N
         """
-        This method returns the velocity change in m/s when the force is applied for
-        time_step_s duration.
+        When the foot/paddle makes contact with the ground initially the board will
+        slowdown due to a negative force.  That force is simply a small fraction of the 
+        total force required by the rider to move the board to velocity_increase_mps.
         """
-        if not self.__push_active:
-            return None
+        self.__F_slowdown_x_axis_N = -(0.10 * self.__F_rider_x_axis_N)
 
+    @property
+    def force_rider_N(self) -> float:
+        """
+        Returns the the force required by the rider to increase the skateboard's velocity
+        to velocity_increase_mps over the time interval of PUSH_TIME_S.
+        """
+        return self.__F_rider_x_axis_N
+
+    @property
+    def slowdown_force_N(self) -> float:
+        """
+        Returns the force that causes a small slowdown in the skateboard's velocity when
+        a push is started by the rider.
+        """
+        return self.__F_slowdown_x_axis_N
+
+    @property
+    def push_active(self) -> bool:
+        """
+        Returns True if the push is active, False otherwise.
+        """
+        return self.__push_active
+
+    def step(self, time_step_s: int) -> tuple[float, float]:
+        """
+        Computes the acceleration and velocity deltas, along the x-axis, for the skateboard over
+        the time_step_s.
+
+        During each time step, a proportion of the rider force is imparted such
+        that over the entire push duration, the total force applied is equivalent to the rider force
+        being applied at every time step over the entire push duration.  This is done to emulate how
+        a the skateboard is accelerated when the rider pushes the skateboard with a foot/paddle.
+        Similarly, the same logic is used in the slowdown interval.
+
+        Args:
+            time_step_s: the time step in seconds used for computing the acceleration and velocity
+        """
+        acceleration_mps2 = None
         velocity_delta_mps = None
-        if self.__elapsed_time_s < self.INITIAL_SLOWDOWN_TIME_S:
-            neg_acceleration_mps2 = (
-                self.__F_slowdown_N / self.eboard.total_weight_with_rider_kg
+        if self.__elapsed_time_s <= self.INITIAL_SLOWDOWN_TIME_S:
+            force_applied_N = 2 * (
+                self.__F_slowdown_x_axis_N
+                - self.__F_slowdown_x_axis_N
+                * (self.__elapsed_time_s / self.INITIAL_SLOWDOWN_TIME_S)
             )
-            velocity_delta_mps = neg_acceleration_mps2 * time_step_s
-        else:
-            acceleration_mps2 = (
-                self.__F_total_N / self.eboard.total_weight_with_rider_kg
-            )
+            acceleration_mps2 = force_applied_N / self.eboard.total_weight_with_rider_kg
             velocity_delta_mps = acceleration_mps2 * time_step_s
-
+        else:
+            fraction = (
+                self.__elapsed_time_s - self.INITIAL_SLOWDOWN_TIME_S
+            ) / self.PUSH_TIME_S
+            force_applied_N = 2 * (fraction * self.__F_rider_x_axis_N)
+            acceleration_mps2 = force_applied_N / self.eboard.total_weight_with_rider_kg
+            velocity_delta_mps = acceleration_mps2 * time_step_s
         self.__elapsed_time_s += time_step_s
-        if self.__elapsed_time_s >= self.PUSH_TIME_S:
+        if self.__elapsed_time_s > (self.PUSH_TIME_S + self.INITIAL_SLOWDOWN_TIME_S):
             self.__push_active = False
-        return velocity_delta_mps
+        return acceleration_mps2, velocity_delta_mps
