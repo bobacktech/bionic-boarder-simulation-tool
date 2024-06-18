ea828ecacabf5f0ee1dd54e2129223908b92faf8
bobacktech <bobacktech@gmail.com>
2024-06-18 16:52:14 -0700

    SIM-43: Made the time step for the push an argument to the constructor.

--- augmented_skateboarding_simulator/riding/push_model.py
+++ augmented_skateboarding_simulator/riding/push_model.py
@@ -6,8 +6,9 @@ class PushModel:
 
     SLOWDOWN_DURATION_FACTOR = 0.10
 
-    def __init__(self, eboard: EBoard) -> None:
+    def __init__(self, eboard: EBoard, time_step_ms: int) -> None:
         self.__eboard = eboard
+        self.__time_step_s = time_step_ms / 1000
 
     def setup(self, force_rider_N: float, push_duration_ms: int) -> None:
         """TBD"""
@@ -40,23 +41,22 @@ class PushModel:
         """
         return int(self.__initial_slowdown_duration_s * 1000)
 
-    def step(self, time_step_ms: int) -> tuple[float, float]:
+    def step(self) -> tuple[float, float]:
         """TBD"""
-        time_step_s = time_step_ms / 1000
         acceleration_ms2, delta_velocity_mps = None, None
         if self.__elapsed_time_s <= self.__initial_slowdown_duration_s:
             acceleration_ms2 = 2 * (
                 self.__rider_slowdown_accel_ms2
                 * (1.0 - (self.__elapsed_time_s / self.__initial_slowdown_duration_s))
             )
-            delta_velocity_mps = acceleration_ms2 * self.__elapsed_time_s
+            delta_velocity_mps = acceleration_ms2 * self.__time_step_s
         else:
             fraction = (
                 self.__elapsed_time_s - self.__initial_slowdown_duration_s
             ) / self.__push_duration_s
             acceleration_ms2 = 2 * (self.__rider_accel_ms2 * fraction)
-            delta_velocity_mps = acceleration_ms2 * self.__elapsed_time_s
-        self.__elapsed_time_s += time_step_s
+            delta_velocity_mps = acceleration_ms2 * self.__time_step_s
+        self.__elapsed_time_s += self.__time_step_s
         if self.__elapsed_time_s > (
             self.__push_duration_s + self.__initial_slowdown_duration_s
         ):
