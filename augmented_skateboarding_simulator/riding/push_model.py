721e83e3550f03484bb85bbf99187bef17a77b55
bobacktech <bobacktech@gmail.com>
2024-06-18 15:06:26 -0700

    SIM-43: Used a setup method in PushModel class so the one instance of the class can be used for multiple pushes.

--- augmented_skateboarding_simulator/riding/push_model.py
+++ augmented_skateboarding_simulator/riding/push_model.py
@@ -4,23 +4,41 @@ from .eboard import EBoard
 class PushModel:
     """TBD"""
 
-    def __init__(
-        self, force_rider_N: float, push_duration_ms: int, eboard: EBoard
-    ) -> None:
-        self.__rider_accel_ms2 = force_rider_N / eboard.total_weight_with_rider_kg
+    SLOWDOWN_DURATION_FACTOR = 0.10
+
+    def __init__(self, eboard: EBoard) -> None:
+        self.__eboard = eboard
+
+    def setup(self, force_rider_N: float, push_duration_ms: int) -> None:
+        """TBD"""
+        self.__rider_accel_ms2 = (
+            force_rider_N / self.__eboard.total_weight_with_rider_kg
+        )
         self.__push_duration_s = push_duration_ms / 1000
-        self.__initial_slowdown_duration_s = 0.10 * self.__push_duration_s
-        self.__rider_slowdown_accel_ms2 = -(0.10 * self.__rider_accel_ms2)
+        self.__initial_slowdown_duration_s = (
+            PushModel.SLOWDOWN_DURATION_FACTOR * self.__push_duration_s
+        )
+        self.__rider_slowdown_accel_ms2 = -(
+            PushModel.SLOWDOWN_DURATION_FACTOR * self.__rider_accel_ms2
+        )
         self.__elapsed_time_s = 0
-        self.__push_finished = False
+        self.__push_active = True
 
     @property
-    def push_finished(self) -> bool:
+    def push_active(self) -> bool:
         """
         Returns:
-            bool: True if the push has finished, False otherwise
+            bool: True if the push is still active, False otherwise
         """
-        return self.__push_finished
+        return self.__push_active
+
+    @property
+    def slowdown_duration_ms(self) -> int:
+        """
+        Returns:
+            int: The duration of the slowdown phase in milliseconds
+        """
+        return int(self.__initial_slowdown_duration_s * 1000)
 
     def step(self, time_step_ms: int) -> tuple[float, float]:
         """TBD"""
@@ -42,5 +60,5 @@ class PushModel:
         if self.__elapsed_time_s > (
             self.__push_duration_s + self.__initial_slowdown_duration_s
         ):
-            self.__push_finished = True
+            self.__push_active = False
         return acceleration_ms2, delta_velocity_mps
