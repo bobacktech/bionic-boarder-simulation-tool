f50ae68561a84f40729472023a140929ddd684e3
bobacktech <bobacktech@gmail.com>
2024-07-06 15:34:02 -0700

    SIM-47: Changed step method to take time step in milliseconds as an input parameter.

--- augmented_skateboarding_simulator/riding/push_model.py
+++ augmented_skateboarding_simulator/riding/push_model.py
@@ -13,7 +13,6 @@ class PushModel:
 
     Attributes:
         __eboard (EBoard): The eboard object associated with this push model.
-        __time_step_s (float): The time step in seconds.
         __rider_accel_ms2 (float): The rider acceleration in m/s^2.
         __push_duration_s (float): The duration of the push in seconds.
         __initial_slowdown_duration_s (float): The initial slowdown duration in seconds.
@@ -24,9 +23,8 @@ class PushModel:
 
     SLOWDOWN_DURATION_FACTOR = 0.10
 
-    def __init__(self, eboard: EBoard, time_step_ms: int) -> None:
+    def __init__(self, eboard: EBoard) -> None:
         self.__eboard = eboard
-        self.__time_step_s = time_step_ms / 1000
 
     def setup(self, force_rider_N: float, push_duration_ms: int) -> None:
         """
@@ -39,16 +37,10 @@ class PushModel:
         Returns:
             None
         """
-        self.__rider_accel_ms2 = (
-            force_rider_N / self.__eboard.total_weight_with_rider_kg
-        )
+        self.__rider_accel_ms2 = force_rider_N / self.__eboard.total_weight_with_rider_kg
         self.__push_duration_s = push_duration_ms / 1000
-        self.__initial_slowdown_duration_s = (
-            PushModel.SLOWDOWN_DURATION_FACTOR * self.__push_duration_s
-        )
-        self.__rider_slowdown_accel_ms2 = -(
-            PushModel.SLOWDOWN_DURATION_FACTOR * self.__rider_accel_ms2
-        )
+        self.__initial_slowdown_duration_s = PushModel.SLOWDOWN_DURATION_FACTOR * self.__push_duration_s
+        self.__rider_slowdown_accel_ms2 = -(PushModel.SLOWDOWN_DURATION_FACTOR * self.__rider_accel_ms2)
         self.__elapsed_time_s = 0
         self.__push_active = True
 
@@ -68,7 +60,7 @@ class PushModel:
         """
         return int(self.__initial_slowdown_duration_s * 1000)
 
-    def step(self) -> tuple[float, float]:
+    def step(self, time_step_ms: int) -> tuple[float, float]:
         """
         To emulate reality, the rider's acceleration is not all applied at once for the
         entire push. Instead, the acceleration is increased over time linearly such that it
@@ -77,25 +69,23 @@ class PushModel:
         For the initial slowdown phase, the acceleration is decreased linearly following the same
         approach as above.
 
+        Args:
+            time_step_ms (int): The time step in milliseconds
         Returns:
             tuple: A tuple containing the acceleration in m/s^2 and the delta velocity in m/s
         """
+        t_sec = time_step_ms / 1000.0
         acceleration_ms2, delta_velocity_mps = None, None
         if self.__elapsed_time_s <= self.__initial_slowdown_duration_s:
             acceleration_ms2 = 2 * (
-                self.__rider_slowdown_accel_ms2
-                * (1.0 - (self.__elapsed_time_s / self.__initial_slowdown_duration_s))
+                self.__rider_slowdown_accel_ms2 * (1.0 - (self.__elapsed_time_s / self.__initial_slowdown_duration_s))
             )
-            delta_velocity_mps = acceleration_ms2 * self.__time_step_s
+            delta_velocity_mps = acceleration_ms2 * t_sec
         else:
-            fraction = (
-                self.__elapsed_time_s - self.__initial_slowdown_duration_s
-            ) / self.__push_duration_s
+            fraction = (self.__elapsed_time_s - self.__initial_slowdown_duration_s) / self.__push_duration_s
             acceleration_ms2 = 2 * (self.__rider_accel_ms2 * fraction)
-            delta_velocity_mps = acceleration_ms2 * self.__time_step_s
-        self.__elapsed_time_s += self.__time_step_s
-        if self.__elapsed_time_s > (
-            self.__push_duration_s + self.__initial_slowdown_duration_s
-        ):
+            delta_velocity_mps = acceleration_ms2 * t_sec
+        self.__elapsed_time_s += t_sec
+        if self.__elapsed_time_s > (self.__push_duration_s + self.__initial_slowdown_duration_s):
             self.__push_active = False
         return acceleration_ms2, delta_velocity_mps
