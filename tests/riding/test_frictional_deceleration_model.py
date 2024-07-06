2725ca7b90ccefedcdc0a1baa6d2e68e913b4367
bobacktech <bobacktech@gmail.com>
2024-07-05 18:55:55 -0700

    SIM-46: Updated unit test for test_decelerate method.

--- tests/riding/test_frictional_deceleration_model.py
+++ tests/riding/test_frictional_deceleration_model.py
@@ -12,18 +12,22 @@ class TestFrictionalDecelerationModel:
         fdm = FrictionalDecelerationModel(mu_rolling=0.01, c_drag=0.90, eboard=eboard)
         current_velocity_m_per_s = 1000 / 3600
         time_step_ms = (20, 40, 60, 80, 100)
-        temp = current_velocity_m_per_s
+        temp_cv_m_per_s = current_velocity_m_per_s
         for t in time_step_ms:
-            new_velocity_m_per_s = fdm.decelerate(current_velocity_m_per_s, t)
-            assert new_velocity_m_per_s < temp
-            temp = new_velocity_m_per_s
+            accel_ms2, delta_v_m_per_s = fdm.decelerate(current_velocity_m_per_s, t)
+            assert accel_ms2 > 0
+            new_velocity_m_per_s = temp_cv_m_per_s - delta_v_m_per_s
+            assert new_velocity_m_per_s < temp_cv_m_per_s
+            temp_cv_m_per_s = new_velocity_m_per_s
 
-        current_velocity_m_per_s = 0
-        new_velocity_m_per_s = fdm.decelerate(current_velocity_m_per_s, 20)
-        assert new_velocity_m_per_s == 0
-
-        current_velocity_m_per_s = 1000 / 3600
+        initial_velocity_m_per_s = 1000 / 3600
+        current_velocity_m_per_s = initial_velocity_m_per_s
+        accel_ms2, delta_v_m_per_s = fdm.decelerate(current_velocity_m_per_s, 20)
+        current_velocity_m_per_s -= delta_v_m_per_s
         for i in range(10):
-            new_velocity_m_per_s = fdm.decelerate(current_velocity_m_per_s, 20)
-            assert new_velocity_m_per_s < current_velocity_m_per_s
-            current_velocity_m_per_s = new_velocity_m_per_s
+            a, v = fdm.decelerate(current_velocity_m_per_s, 20)
+            assert a > 0
+            current_velocity_m_per_s -= v
+            assert v < delta_v_m_per_s
+            delta_v_m_per_s = v
+        assert current_velocity_m_per_s < initial_velocity_m_per_s
