94b73d2a4453b0a4f9b8630114ab0dfb3b764feb
bobacktech <bobacktech@gmail.com>
2024-08-24 11:59:03 -0700

    SIM-65: Defined the AppInputArguments class for the simulation

--- augmented_skateboarding_simulator/main.py
+++ augmented_skateboarding_simulator/main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from sim_time_manager import SimTimeManager
 import argparse
 import re
@@ -7,11 +8,47 @@ import sys
 from riding import motor_state
 
 
+@dataclass
+class AppInputArguments:
+
+    # Electric Skateboard Specifics
+    total_weight_with_rider_kg: float
+    frontal_area_of_rider_m2: float
+    wheel_diameter_m: float
+    battery_max_capacity_Ah: float
+    battery_max_voltage: float
+    gear_ratio: float
+    motor_kv: int
+    motor_max_torque: float
+    motor_max_amps: float
+    motor_max_power_watts: float
+    motor_pole_pairs: int
+
+    # Serial I/O
+    com_port: str
+    baud_rate: int
+
+    # Kinematic loop
+    fixed_time_step_ms: int
+    push_period_sec: float
+    theta_slope_period_sec: float
+    slope_range_bound_deg: float
+
+    # Friction model
+    mu_rolling: float
+    c_drag: float
+
+    # Motor Controller
+    control_time_step_sec: float
+
+    # VESC
+    vesc_fw: str
+    heartbeat_timeout_sec: float
+
+
 def firmwareRegex(argValue, pattern=re.compile(r"^\d*[.]\d*$")):
     if not pattern.match(argValue):
-        raise argparse.ArgumentTypeError(
-            "VESC firmware version specified as $MajorVersion.$MinorVersion, e.g 2.18"
-        )
+        raise argparse.ArgumentTypeError("VESC firmware version specified as $MajorVersion.$MinorVersion, e.g 2.18")
     return argValue
 
 
@@ -23,9 +60,7 @@ if __name__ == "__main__":
         help="Specifies the VESC firmware version to be used in the simulation.",
         type=firmwareRegex,
     )
-    parser.add_argument(
-        "--comPort", help="The com port for the attached USB FTDI module."
-    )
+    parser.add_argument("--comPort", help="The com port for the attached USB FTDI module.")
     args = parser.parse_args()
     com_port = args.comPort
     vesc_fw = args.vescFW
