a1c440a0cb87dbff95a24c2506e5df65f9d78290
bobacktech <bobacktech@gmail.com>
2024-09-29 15:21:27 -0700

    SIM-69: Updated start_sim_fixture to check if USB FTDI adapter is connected to the PC and then skip the test if it is not connected.

--- tests/functional/start_sim_fixture.py
+++ tests/functional/start_sim_fixture.py
@@ -2,10 +2,22 @@ import subprocess
 import os
 import pytest
 import time
+from pyftdi.ftdi import Ftdi
 
 
 @pytest.fixture(scope="function")
 def start_sim_process():
+    try:
+        # List all connected FTDI devices
+        devices = Ftdi.list_devices()
+        if not devices:
+            pytest.skip("USB FTDI adapter for HC06 bluetooth is not connected to PC. Test case is skipped.")
+    except pytest.skip.Exception:
+        raise
+    except:
+        # An exception will be thrown if the FTDI USB adapter is connected to the PC due to an access privilege issue.
+        # This verifies that the FTDI USB adapter is connected and accessible to the simulation.
+        pass
     command = [
         "poetry",
         "run",
