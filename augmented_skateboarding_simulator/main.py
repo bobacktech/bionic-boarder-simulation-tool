f141798163b7c47533a954214a8e70633459cec8
bobacktech <bobacktech@gmail.com>
2024-10-23 17:33:41 -0700

    SIM-94: Removed the unnecessary argument number check.

--- augmented_skateboarding_simulator/main.py
+++ augmented_skateboarding_simulator/main.py
@@ -53,9 +53,6 @@ if __name__ == "__main__":
     parser.add_argument("app_inputs_json", type=str, help="This is the path to the simulation app inputs file.")
     parser.add_argument("--enable-logging", action="store_true", help="Enable logging if this flag is set.")
     args = parser.parse_args()
-    if len(sys.argv) != 2:
-        print("Error: Exactly one argument is required.")
-        sys.exit(1)
     Logger.enabled = args.enable_logging
     script_dir = os.path.dirname(__file__)
     schema_path = os.path.join(script_dir, "./app_input_arguments.schema.json")
