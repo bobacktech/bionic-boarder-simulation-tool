43c587311c272edc803dbb25e9a9b9927fe00c71
bobacktech <bobacktech@gmail.com>
2024-10-29 15:25:09 -0700

    SIM-97: Added log info messages in setup and step methods.

--- augmented_skateboarding_simulator/riding/push_model.py
+++ augmented_skateboarding_simulator/riding/push_model.py
@@ -1,4 +1,5 @@
 from .eboard import EBoard
+from augmented_skateboarding_simulator.logger import Logger
 
 
 class PushModel:
@@ -44,6 +45,12 @@ class PushModel:
         self.__rider_slowdown_accel_ms2 = -(PushModel.SLOWDOWN_DURATION_FACTOR * self.__rider_accel_ms2)
         self.__elapsed_time_s = 0
         self.__push_active = True
+        Logger().logger.info(
+            "Skateboard push setup",
+            push_active=self.__push_active,
+            initial_slowdown_duration_ms=(self.__initial_slowdown_duration_s * 1000),
+            rider_accel_ms2=self.__rider_accel_ms2,
+        )
 
     @property
     def push_active(self) -> bool:
@@ -89,4 +96,8 @@ class PushModel:
         self.__elapsed_time_s += t_sec
         if self.__elapsed_time_s > (self.__push_duration_s + self.__initial_slowdown_duration_s):
             self.__push_active = False
+            Logger().logger.info(
+                "Skateboard push finished",
+                push_active=self.__push_active,
+            )
         return acceleration_ms2, delta_velocity_mps
