9cda4210a45051ade11478982e84224a60fb90ff
bobacktech <bobacktech@gmail.com>
2025-12-22 16:06:50 -0800

    SIM-145: Added a functional test case to get Bionic Boarder State responses from the simulation over a BLE connection.

--- tests/functional/test_bionic_boarder_command.py
+++ tests/functional/test_bionic_boarder_command.py
@@ -1,8 +1,9 @@
 from . import bionic_boarder_msg_requester
 import pytest
 import time
+import asyncio
 
-NUMBER_VESC_BIONIC_BOARDER_MSG_REQUESTS = 40
+NUMBER_VESC_BIONIC_BOARDER_MSG_REQUESTS = 10
 
 
 @pytest.mark.skip(reason="This test is currently disabled.")
@@ -18,3 +19,18 @@ def test_bionic_boarder_command_response(activate_sim_and_bluetooth_socket):
         bbmr.send_bionic_boarder_msg_request()
         count += 1
     assert len(bbmr.bionic_boarder_msg_buffer) == 40
+
+
+@pytest.mark.asyncio
+async def test_bionic_boarder_command_response_ble(activate_sim_and_ble_client):
+    client = activate_sim_and_ble_client
+    bb_requester = bionic_boarder_msg_requester.VescBionicBoarderMsgRequesterBLE(client)
+    await bb_requester.set_up_response_handler()
+    await bb_requester.send_command()
+    i = 0
+    while i < NUMBER_VESC_BIONIC_BOARDER_MSG_REQUESTS:
+        while bb_requester.response_received == False:
+            await asyncio.sleep(0.1)
+        await bb_requester.send_command()
+        i += 1
+    assert len(bb_requester.response_buffer) == 10
