41318d80ea2442cf4b7e14bab7329b2e94214195
bobacktech <bobacktech@gmail.com>
2026-01-03 14:28:32 -0800

    SIM-153: Added pytest skip annotations to all functional tests.

--- tests/functional/test_vesc_state_command.py
+++ tests/functional/test_vesc_state_command.py
@@ -6,6 +6,7 @@ import asyncio
 NUMBER_VESC_STATE_MSG_REQUESTS = 10
 
 
+@pytest.mark.skip(reason="Skip is the default behavior. Remove this line to enable the test.")
 @pytest.mark.asyncio
 async def test_bionic_boarder_command_response(activate_sim_and_ble_client):
     client = activate_sim_and_ble_client
